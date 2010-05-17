0133b06224ed744692324034a9dc70459e8b6d41
NAntunes <nmsantunes@gmail.com>
2010-05-17 21:51:21 +0100

    Adding some more features to the scripts.

--- openchord-graphdht/script/graph-dht-init.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -classpath graph-dht-init.jar;config;lib/openchord_1.0.5.jar;lib./log4j.jar org.graphdht.openchord.Init
---
+++ openchord-graphdht/script/graphdht-oc-join.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+-- 
+java -classpath graphdht-oc.jar:config:lib/openchord_1.0.5.jar:lib/log4j.jar org.graphdht.openchord.Join
+
+
