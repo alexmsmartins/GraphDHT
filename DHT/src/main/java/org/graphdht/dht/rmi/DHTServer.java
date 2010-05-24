e4963895bceaa21f7f60c05eeea458e4f646f9fb
Alexandre Martins <alexmsmartins@gmail.com>
2010-05-24 15:49:05 +0100

    Adding DHT project again since DHT/src/main/java/org/graphdht/dht/rmi/DHTService.java is the predefined interface between neo4j and chord.

---
+++ DHT/src/main/java/org/graphdht/dht/rmi/DHTServer.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.graphdht.dht.rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Map;
+import org.graphdht.dht.obj.Key;
+import org.graphdht.dht.obj.Value;
+
+/**
+ *
+ * @param <Key>
+ * @param <Value>
+ * @author root
+ */
+public class DHTServer extends UnicastRemoteObject implements DHTService<Key, Value> {
+    //TODO from Alexandre -> the code should be using generics instead of Key and Value classes.
+
+    public DHTServer() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public Value get(Key key) {
+        System.out.println("To implement!!!");
+        return null;
+    }
+
+    @Override
+    public Value put(Key key, Value value) {
+        System.out.println("To implement!!!");
+        return null;
+    }
+
+    @Override
+    public Value remove(Key key) {
+        System.out.println("To implement!!!");
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<Key, Value> m) {
+        System.out.println("To implement!!!");
+    }
+}
