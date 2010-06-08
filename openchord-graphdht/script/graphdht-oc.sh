ef42121198ee4760b2fb31a7fe65fa13e193ba34
Alexandre Martins <alexmsmartins@gmail.com>
2010-06-08 15:24:34 +0100

    Changed script/graphdht-oc.sh to only create 2 chord nodes. It is easier for testing purposes.

--- openchord-graphdht/script/graphdht-oc.sh
+++ openchord-graphdht/script/graphdht-oc.sh
@@ -1,11 +1,3 @@
 xterm -e "java -cp graphdht-oc.jar:config:lib/openchord_1.0.5.jar:lib/log4j.jar org.graphdht.openchord.Init localhost 5000"&
 sleep 1
 xterm -e "java -cp graphdht-oc.jar:config:lib/openchord_1.0.5.jar:lib/log4j.jar org.graphdht.openchord.Join localhost 5000 localhost 5001"&
-sleep 1
-xterm -e "java -cp graphdht-oc.jar:config:lib/openchord_1.0.5.jar:lib/log4j.jar org.graphdht.openchord.Join localhost 5000 localhost 5002"&
-sleep 1
-xterm -e "java -cp graphdht-oc.jar:config:lib/openchord_1.0.5.jar:lib/log4j.jar org.graphdht.openchord.Join localhost 5000 localhost 5003"&
-sleep 1
-xterm -e "java -cp graphdht-oc.jar:config:lib/openchord_1.0.5.jar:lib/log4j.jar org.graphdht.openchord.Join localhost 5000 localhost 5004"&
-sleep 1
-xterm -e "java -cp graphdht-oc.jar:config:lib/openchord_1.0.5.jar:lib/log4j.jar org.graphdht.openchord.Join localhost 5000 localhost 5005"&
